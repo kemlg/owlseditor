69a5f2724a628d598e2474d6ef8fce8870f85ee9
Sergio Alvarez-Napagao <salvarez@lsi.upc.edu>
2013-05-03 05:36:08 +0200

    moving owls2wsdl to other repo

--- owls2wsdl/parseont.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#
-# Shell script for DFKI linux environment only.
-#
-
-# java -Xms : minimmal zur Verfuegung stehender Speicherplatz
-# java -Xmx : maximaal zur Verfuegung stehender Speicherplatz
-/home/ofourman/jre1.5.0_11/bin/java -Xms256m -Xmx512m -jar OWLS2WSDL.jar -kbdir /home/ofourman/owls2wsdl_kbs $1 
--- owls2wsdl/rungui.sh
+++
@@ -1,5 +0,0 @@
-#
-# Shell script for DFKI linux environment only.
-#
-
-/home/ofourman/jre1.5.0_11/bin/java -jar OWLS2WSDL.jar &
